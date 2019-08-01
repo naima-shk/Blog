1366ac8de8ba3c7dce644aaa8a935b0e13303fa4
profitr <ruben.profit@dowjones.com>
2019-08-01 08:32:19 -0400

    created about and contact page

---
+++ Frontend/src/pages/About/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Naima's Blog :)
+      </header>
+    </div>
+  );
+}
+
+export default About;
