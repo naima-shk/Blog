0f3244189017e13b11b3c8122e6092943c4e438f
naima shaikh <naimashaikh550@gmail.com>
2019-08-02 16:03:35 +0500

    'upgrading'

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,16 +1,41 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import "./styles/App.css";
+import {Link,Navigation} from "react-router-dom";
+//import "./styles/App.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Social from "./pages/Social";
 import Contact from "./pages/Contact";
 import Blog from "./pages/Blog";
+import styled from 'styled-components';
+
+
+
+const Home =styled.Home
+  font-family:sans-serif ;
+  font-size: 1.3rem;
+  border: none;
+  border-radius:5px;
+  padding: 8px ,7px;
+
+
+
 
 const App = () => {
   return (
     <div className="App">
+       <Navigation>
+      <Link >
+      <Link to="/"> Home</Link>
+      <Link to="/About">About</Link>
+      <Link to="/Contact">Contact</Link>
+      <Link to="/Projects">Projects</Link>
+      <Link to="/Blog">Blog</Link>
+      <Link to="/Social">Social</Link>
+      </Link>
+      </Navigation>
+      
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/About" component={About} />
