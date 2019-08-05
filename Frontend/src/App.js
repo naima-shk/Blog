9b493aa37c3437c1decf614ba536ac838d73f402
profitr <ruben.profit@dowjones.com>
2019-08-05 16:08:27 -0400

    direct link to email

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,7 +4,6 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Social from "./pages/Social";
-import Contact from "./pages/Contact";
 import Blog from "./pages/Blog";
 import styled from "styled-components";
 
@@ -19,16 +18,21 @@ const NavLink = styled(Link)`
   background: papayawhip;
 `;
 
+const Contact = styled.a`
+  padding: 4em;
+  background: papayawhip;
+`
+
 const App = () => {
   return (
     <div className="App">
       <Nav>
         <NavLink to="/"> Home</NavLink>
         <NavLink to="/About">About</NavLink>
-        <NavLink to="/Contact">Contact</NavLink>
         <NavLink to="/Projects">Projects</NavLink>
         <NavLink to="/Blog">Blog</NavLink>
         <NavLink to="/Social">Social</NavLink>
+        <Contact href="mailto:naimashaikh550@gmail.com?subject=Contact Form">Contact</Contact>
       </Nav>
 
       <Switch>
@@ -37,7 +41,6 @@ const App = () => {
         <Route exact path="/Projects" component={Projects} />
         <Route exact path="/Blog" component={Blog} />
         <Route exact path="/Social" component={Social} />
-        <Route exact path="/Contact" component={Contact} />
       </Switch>
     </div>
   );
