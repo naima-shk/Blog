7b35dfe8ac3574a78b0ccdcb59050bc05d146a60
Ruben Profit <rubenprofit@ac.c4q.nyc>
2019-08-09 13:35:50 -0400

    added home link to about page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -7,10 +7,6 @@ import Social from "./pages/Social";
 import Blog from "./pages/Blog";
 import styled from "styled-components";
 
-  
-
-
-
 const Container = styled.div`
   height: 100vh;
   display: flex;
@@ -18,11 +14,8 @@ const Container = styled.div`
   justify-content: center;
 `;
 
-
-
 const App = () => {
   return (
-    
     <Container>
       <Switch>
         <Route exact path="/" component={Home} />
@@ -31,9 +24,8 @@ const App = () => {
         <Route exact path="/Blog" component={Blog} />
         <Route exact path="/Social" component={Social} />
       </Switch>
-      
     </Container>
   );
 };
 
-export default App  ;
+export default App;
