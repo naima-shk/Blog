86556f7fe2ed60eafc1ef3390d8ef0d6f6b01784
Ruben Profit <rubenprofit@ac.c4q.nyc>
2019-08-11 14:48:49 -0400

    about update

--- Frontend/src/pages/About/index.js
+++ Frontend/src/pages/About/index.js
@@ -1,32 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import BackLink from "../../components/BackLink";
 
 const Container = styled.div`
   display: block;
   flex-direction: column;
-  height: 700vh;
+  height: 100vh;
 `;
 
-const Text = styled.p`
+const MainText = styled.h1`
   display: block;
   justify-content: center;
-  padding : 10em;
-  background :#A55959;
-  font-family:  Georgia, serif;
-  text-transform:  capitalize;
-  font-size:20px;
-  font-color: #FFFFFF;
-  `;
+  margin: 25px 0 10px 25px;
+  font-size: 20px;
+  color: #a55959;
+`;
+
+const SubText = styled.p`
+  display: block;
+  justify-content: center;
+  margin: 25px 0 10px 25px;
+  font-size: 16px;
+  color: #a55959;
+`;
+
+const Quote = styled.b`
+  display: flex;
+  justify-content: flex-start;
+  margin-left: 25px;
+  cursor: pointer;
+`;
+
+const SkillsList = styled.ul`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+const Skill = styled.li`
+  display: block;
+  justify-content: center;
+  margin-right: 10px;
+  font-size: 14px;
+  color: #a55959;
+`;
+
+const allQuotes = [
+  `"Great things are done by a series of small things brought together."`,
+  `"Whatever you are, be a good one."`,
+  `"If you dream it, you can do it."`,
+  `"Life is trying things to see if they work."`,
+  `"Believe you can and you’re halfway there."`,
+  `"What you seek is seeking you."`
+];
+
+const skills = ["React", "HTML", "CSS", "Node.js", "PostgreSQL", "SQL"];
 
 const About = () => {
+  const [quote, setQuote] = useState(allQuotes[5]);
+
+  const changeQuote = () =>
+    setQuote(allQuotes[Math.floor(Math.random() * allQuotes.length)]);
+
   return (
     <Container>
       <BackLink />
-      <Text >
-       <b>Great things are done by a series of small things brought together.</b>
-      I am a Freelance Javascipt Developer from Pakistan  . I build websites using Javascipt more  especifically Reactjs , Postgre sql , styled components
-      </Text>
+      <Quote onClick={changeQuote}>{quote}</Quote>
+      <MainText>I'm a Javascipt Developer from Pakistan.</MainText>
+      <SubText>I build applications and tools for the Web on the Web.</SubText>
+      <SubText>Skills:</SubText>
+      <SkillsList>
+        {skills.map(skill => {
+          return <Skill>{skill}</Skill>;
+        })}
+      </SkillsList>
     </Container>
   );
 };
