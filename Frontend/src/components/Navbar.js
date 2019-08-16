ed9ebf630ac7a0df5720cdc87f5bb4e67ba6d349
profitr <ruben.profit@dowjones.com>
2019-08-16 08:58:03 -0400

    fix

--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -2,9 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const Container = styled.div`
-
-`;
+const Container = styled.div``;
 
 const Nav = styled.nav`
   display: flex;
