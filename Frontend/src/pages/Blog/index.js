7f88821aac987a6d606e0ded150e5629ac86646d
Ruben Profit <rubenprofit@ac.c4q.nyc>
2019-09-03 06:46:18 -0400

    ejected

--- Frontend/src/pages/Blog/index.js
+++ Frontend/src/pages/Blog/index.js
@@ -1,11 +1,8 @@
-import React, { lazy, Suspense, useEffect } from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import BackLink from "../../components/BackLink";
-import { importMDX } from 'mdx.macro';
-import { metadata } from "../../assets/blog-posts/first-post/index.mdx";
-
-const Content = lazy(() => importMDX("../../assets/blog-posts/first-post/index.mdx"));
+import MDXDocument, { metadata } from "../../assets/blog-posts/first-post/index.mdx";
 
 const Container = styled.div`
   display: inline;
@@ -25,17 +22,16 @@ const LinkToPost = styled(Link)`
 
 const Blog = () => {
   useEffect(() => {
-    console.log('document', Content)
+    console.log('document', MDXDocument)
     console.log('metadata', metadata)
-  })
+  });
+
   return (
     <Container>
       <BackLink />
       <Text>My Blog posts!</Text>
       <LinkToPost to="/blog/1">First Post!</LinkToPost>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Content />
-      </Suspense>
+      {/* <MDXDocument /> */}
     </Container>
   );
 };
