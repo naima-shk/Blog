3796ba32eeb94676757540baa447bc9f6e8c2b7f
Ruben Profit <rubenprofit@ac.c4q.nyc>
2019-09-05 22:07:12 -0400

    deleted first page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,7 +3,6 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Blog from "./pages/Blog";
-import FirstPost from "./pages/First-Post";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -25,7 +24,6 @@ const App = () => {
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/blog" component={Blog} />
-        <Route exact path="/blog/1" component={FirstPost} />
       </Switch>
     </Container>
   );
