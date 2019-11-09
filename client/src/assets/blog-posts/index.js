e1e6ba61f0251912c909a609e0da39b7f889dce8
naima shaikh <naimashaikh550@gmail.com>
2019-11-09 15:17:05 +0500

    'editing'

--- client/src/assets/blog-posts/index.js
+++ client/src/assets/blog-posts/index.js
@@ -1,5 +1,3 @@
 import { metadata as firstPostMetdata } from "./first-post/index.mdx";
 import { metadata as outreachyExperienceMetadata } from "./outreachy-experience/index.mdx"
-export default [
-    firstPostMetdata,outreachyExperienceMetadata
-]
+export default [firstPostMetdata,outreachyExperienceMetadata]
